118e7f39f429ccda9bbc2cb8ceed6259996710b3
David Kim <cjoak1028@gmail.com>
2022-03-02 21:42:39 -0500

    fixed toggle button in dark mode

--- script.js
+++ script.js
@@ -1,31 +1,12 @@
 const bodyElem = document.body;
 const toggleButton = document.querySelector('.mode-toggle');
-const toggleButtonIcon = document.querySelector('.mode-toggle img')
+const toggleButtonLabel = document.querySelector('.mode-toggle__title')
 
 toggleButton.addEventListener('click', () => {
     bodyElem.classList.toggle('dark-mode');
     if (bodyElem.classList.contains('dark-mode')) {
-        toggleButtonIcon.src = './assets/dark-mode/icon-sun.svg'
+        toggleButtonLabel.innerHTML = 'light';
     } else {
-        toggleButtonIcon.src = './assets/light-mode/icon-moon.svg'
-    }
-})
-
-// BUGGY
-toggleButton.addEventListener('mouseover', () => {
-    // Change toggle icon accordingly
-    if (bodyElem.classList.contains('dark-mode')) {
-        toggleButtonIcon.src = './assets/dark-mode/icon-sun-hover.svg'
-    } else {
-        toggleButtonIcon.src = './assets/light-mode/icon-moon-hover.svg'
-    }
-})
-
-toggleButton.addEventListener('mouseout', () => {
-    // Change toggle icon accordingly
-    if (bodyElem.classList.contains('dark-mode')) {
-        toggleButtonIcon.src = './assets/dark-mode/icon-sun.svg'
-    } else {
-        toggleButtonIcon.src = './assets/light-mode/icon-moon.svg'
+        toggleButtonLabel.innerHTML = 'dark';
     }
 })
