3ebc952129a23006991ccaf099f1875542662c61
David Kim <cjoak1028@gmail.com>
2022-03-03 22:59:11 -0500

    updates most profile info

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 'use strict';
 
-var token = config.MY_API_TOKEN;
-
 import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
-const octokit = new Octokit({ auth: token });
+const octokit = new Octokit({ auth: config.MY_API_TOKEN });
 
 const bodyElem = document.body;
 const toggleButton = document.querySelector('.mode-toggle');
 const toggleButtonLabel = document.querySelector('.mode-toggle__title');
 const searchForm = document.querySelector('.form-group');
 const searchInput = document.querySelector('.form-control');
+
+// const userPhoto = document.querySelector('.user-profile__photo img');
+const userName = document.querySelector('.user-name');
+const userId = document.querySelector('.user-id');
+const userBio = document.querySelector('.user-bio');
+
+const repos = document.querySelector('.repos .stat-data');
+const followers = document.querySelector('.followers .stat-data');
+const following = document.querySelector('.following .stat-data');
+
+const location = document.querySelector('.location');
+const website = document.querySelector('.website');
+const twitter = document.querySelector('.twitter');
+const company = document.querySelector('.company');
+
+
 
 const getUserData = async (username) => {
     const userData = await octokit.request('GET /users/{username}', {
@@ -21,6 +35,54 @@ const getUserData = async (username) => {
 
 const updateProfile = (data) => {
     console.log(data);
+    // userPhoto.src = data.avatar_url;
+    userName.innerHTML = data.name || data.login;
+    userId.innerHTML = `@${data.login}`;
+    // userBio.innerHTML = data.bio || 'This profile has no bio';
+    if (!data.bio) {
+        userBio.innerHTML = 'This profile has no bio';
+        userBio.classList.add('not-available');
+    } else {
+        userBio.innerHTML = data.bio;
+        userBio.classList.remove('not-available');
+    }
+
+    repos.innerHTML = data.public_repos;
+    followers.innerHTML = data.followers;
+    following.innerHTML = data.following;
+
+    if (!data.location) {
+        location.classList.add('not-available');
+        location.querySelector('p').innerHTML = 'Not Available';
+    } else {
+        location.classList.remove('not-available');
+        location.querySelector('p').innerHTML = data.location;
+    }
+
+    if (!data.blog) {
+        website.classList.add('not-available');
+        website.querySelector('p').innerHTML = 'Not Available';
+    } else {
+        website.classList.remove('not-available');
+        website.querySelector('p').innerHTML = data.blog;
+    }
+
+    if (!data.twitter_username) {
+        twitter.classList.add('not-available');
+        twitter.querySelector('p').innerHTML = 'Not Available';
+    } else {
+        twitter.classList.remove('not-available');
+        twitter.querySelector('p').innerHTML = data.twitter_username;
+    }
+
+    if (!data.company) {
+        company.classList.add('not-available');
+        company.querySelector('p').innerHTML = 'Not Available';
+    } else {
+        company.classList.remove('not-available');
+        company.querySelector('p').innerHTML = data.company;
+    }
+
 }
 
 toggleButton.addEventListener('click', () => {
@@ -39,6 +101,7 @@ searchForm.addEventListener('submit', (e) => {
         return;
     }
 
+    // PLACE IN SEPARATE FUNCTION
     getUserData(searchInput.value)
         .then((user) => {
             updateProfile(user.data);
@@ -47,3 +110,16 @@ searchForm.addEventListener('submit', (e) => {
             console.error(err);
         });
 });
+
+const init = () => {
+    // PLACE IN SEPARATE FUNCTION
+    getUserData('octocat')
+        .then((user) => {
+            updateProfile(user.data);
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+}
+
+init();
