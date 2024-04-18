4bb01f4f96ea214fd5a1b4777b2e4fbf3b210d08
Chang-Ju (David) Kim <cjoak1028@gmail.com>
2024-04-17 22:04:37 -0400

    Refactored

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-const themeToggler = document.querySelector('.theme-toggler');
+// const themeToggler = document.querySelector('.theme-toggler');
 
 // Function that updates styles of elements when theme is toggled
-function toggleTheme() {
-    const togglerLabel = this.querySelector('.theme-toggler__label');
-    const togglerIcon = this.querySelector('.theme-toggler__icon');
-    const body = document.body;
-    const icons = document.querySelectorAll('.icon');
+// function toggleTheme() {
+//     const togglerLabel = this.getElementById('theme-label');
+//     const togglerIcon = this.querySelector('.theme-toggler__icon');
+//     const body = document.body;
+//     const icons = document.querySelectorAll('.icon');
 
-    // Toggle theme class
-    body.classList.toggle('theme-light');
-    body.classList.toggle('theme-dark');
+//     // Toggle theme class
+//     body.classList.toggle('theme-light');
+//     body.classList.toggle('theme-dark');
 
-    // Change theme toggler button based on selected theme
-    togglerLabel.textContent = body.classList.contains('theme-light') ? 'Dark' : 'Light';
-    togglerIcon.setAttribute('src', body.classList.contains('theme-light') ? './assets/icon-moon.svg' : './assets/icon-sun.svg');
+//     // Change theme toggler button based on selected theme
+//     togglerLabel.textContent = body.classList.contains('theme-light') ? 'Dark' : 'Light';
+//     togglerIcon.setAttribute('src', body.classList.contains('theme-light') ? './assets/icon-moon.svg' : './assets/icon-sun.svg');
 
-    // Change icon colours based on selected theme
-    icons.forEach((icon) => {
-        let imgSrc = icon.getAttribute('src');
-        let newImgSrc = body.classList.contains('theme-light')
-            ? imgSrc.replace('-white.svg', '.svg')
-            : imgSrc.replace('.svg', '-white.svg');
+//     // Change icon colours based on selected theme
+//     icons.forEach((icon) => {
+//         let imgSrc = icon.getAttribute('src');
+//         let newImgSrc = body.classList.contains('theme-light')
+//             ? imgSrc.replace('-white.svg', '.svg')
+//             : imgSrc.replace('.svg', '-white.svg');
 
-        icon.setAttribute('src', newImgSrc);
-    });
-}
+//         icon.setAttribute('src', newImgSrc);
+//     });
+// }
 
-themeToggler.addEventListener('click', toggleTheme);
+// themeToggler.addEventListener('click', toggleTheme);
