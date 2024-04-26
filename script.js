739da5fa72bd01e29be65d2fd33d4c76896b8f17
Chang-Ju (David) Kim <cjoak1028@gmail.com>
2024-04-25 21:35:42 -0400

    Refactored script

--- script.js
+++ script.js
@@ -13,10 +13,6 @@ const userBlogEl = document.getElementById('user-blog');
 const userTwitterEl = document.getElementById('user-twitter');
 const userCompanyEl = document.getElementById('user-company');
 const profileImageList = document.getElementsByClassName('profile-image');
-const userLocationContainer = document.getElementById('user-location-container');
-const userBlogContainer = document.getElementById('user-blog-container');
-const userTwitterContainer = document.getElementById('user-twitter-container');
-const userCompanyContainer = document.getElementById('user-company-container');
 
 // Function that updates styles of elements when theme is toggled
 function toggleTheme() {
@@ -26,90 +22,6 @@ function toggleTheme() {
 
     // Change theme toggler button based on selected theme
     togglerLabel.textContent = document.body.classList.contains('light-theme') ? 'Dark' : 'Light';
-}
-
-// Render user information on the UI
-function renderUserInfo(user) {
-    renderUserDetails(user);
-    renderUserSocialLinks(user);
-    console.log(user);
-}
-
-// Extract and render user details
-function renderUserDetails(user) {
-    const { name, login, created_at, bio, avatar_url, location, company } = user;
-
-    userNameEl.innerText = name || login;
-    userLoginEl.innerText = login;
-    userBioEl.innerText = bio || "This profile has no bio";
-    for (let image of profileImageList) {
-        image.setAttribute('src', avatar_url);
-    }
-    renderUserDateJoined(created_at)
-    renderUserLocation(location);
-    renderUserCompany(company);
-}
-
-function renderUserDateJoined(dateJoined) {
-    const formattedDate = formatDate(dateJoined);
-    userDateJoinedEl.innerText = formattedDate;
-
-}
-
-function formatDate(dateString) {
-    const months = [
-        'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
-    ];
-
-    const [year, month, day] = dateString.split('T')[0].split('-');
-    const monthAbbreviation = months[parseInt(month, 10) - 1];
-
-    return `${parseInt(day, 10)} ${monthAbbreviation} ${year}`;
-}
-
-// Extract and render social links
-function renderUserSocialLinks(user) {
-    const { blog, twitter_username } = user;
-
-    renderSocialLink(userBlogEl, blog, "https://", "Not Available");
-    renderSocialLink(userTwitterEl, twitter_username, "https://twitter.com/", "Not Available");
-}
-
-// Set social link attributes and text
-function renderSocialLink(element, link, prefix, fallbackText) {
-    if (link) {
-        element.setAttribute('href', `${prefix}${link}`);
-        element.innerText = link;
-        element.parentElement.classList.remove('not-available');
-    } else {
-        element.setAttribute('href', "");
-        element.innerText = fallbackText;
-        element.parentElement.classList.add('not-available');
-    }
-}
-
-// Render user location
-function renderUserLocation(location) {
-    userLocationEl.innerText = location ? location.split(',')[0] : "Not Available";
-    toggleContainerOpacity(userLocationContainer, location);
-}
-
-// Render user company
-function renderUserCompany(company) {
-    const formattedCompany = company ? `https://github.com/${company.substr(1)}` : "";
-    userCompanyEl.setAttribute('href', formattedCompany);
-    userCompanyEl.innerText = company || "Not Available";
-    toggleContainerOpacity(userCompanyContainer, company);
-}
-
-// Handle opacity of link containers
-function toggleContainerOpacity(container, condition) {
-    if (condition) {
-        container.classList.remove('not-available');
-    } else {
-        container.classList.add('not-available');
-    }
 }
 
 // Fetches user based on username and updates DOM
@@ -123,6 +35,96 @@ async function fetchUser(username) {
     }
 }
 
+// Render user information on the UI
+function renderUserInfo(user) {
+    renderUserDetails(user);
+    renderUserLinks(user);
+    console.log(user);
+}
+
+// Extract and render user details
+function renderUserDetails(user) {
+    const { name, login, created_at, bio, avatar_url } = user;
+
+    userNameEl.innerText = name || login;
+    userLoginEl.innerText = login;
+    userBioEl.innerText = bio || "This profile has no bio";
+    for (let image of profileImageList) {
+        image.setAttribute('src', avatar_url);
+    }
+    renderUserDateJoined(created_at)
+}
+
+// Render user date joined
+function renderUserDateJoined(dateJoined) {
+    const formattedDate = formatDate(dateJoined);
+    userDateJoinedEl.innerText = formattedDate;
+}
+
+// Converts date format
+function formatDate(dateString) {
+    const months = [
+        'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+    ];
+
+    const [year, month, day] = dateString.split('T')[0].split('-');
+    const monthAbbreviation = months[parseInt(month, 10) - 1];
+
+    return `${parseInt(day, 10)} ${monthAbbreviation} ${year}`;
+}
+
+// Extract and render social links
+function renderUserLinks(user) {
+    const { location, company, blog, twitter_username } = user;
+
+    renderUserLocation(location);
+    renderUserCompany(company);
+    renderUserBlog(blog);
+    renderUserTwitter(twitter_username);
+}
+
+// Render user location
+function renderUserLocation(location) {
+    userLocationEl.innerText = location ? location.split(',')[0] : "Not Available";
+    location ? userLocationEl.parentElement.classList.remove('not-available') : userLocationEl.parentElement.classList.add('not-available');
+}
+
+// Render user company
+function renderUserCompany(company) {
+    const formattedCompany = company ? `https://github.com/${company.substr(1)}` : "";
+    userCompanyEl.setAttribute('href', formattedCompany);
+    userCompanyEl.innerText = company || "Not Available";
+
+    company ? userCompanyEl.parentElement.classList.remove('not-available') : userCompanyEl.parentElement.classList.add('not-available');
+}
+
+// Render user twitter
+function renderUserTwitter(username) {
+    if (username) {
+        userTwitterEl.setAttribute('href', `https://twitter.com/${username}`);
+        userTwitterEl.innerText = username;
+        userTwitterEl.parentElement.classList.remove('not-available');
+    } else {
+        userTwitterEl.setAttribute('href', "");
+        userTwitterEl.innerText = "Not Available";
+        userTwitterEl.parentElement.classList.add('not-available');
+    }
+}
+
+// Render user blog
+function renderUserBlog(blog) {
+    if (blog) {
+        userBlogEl.setAttribute('href', `https://${blog}`);
+        userBlogEl.innerText = blog;
+        userBlogEl.parentElement.classList.remove('not-available');
+    } else {
+        userBlogEl.setAttribute('href', "");
+        userBlogEl.innerText = "Not Available";
+        userBlogEl.parentElement.classList.add('not-available');
+    }
+}
+
 themeToggler.addEventListener('click', toggleTheme);
 
-fetchUser('cjoak1028');
+fetchUser('stripe');
